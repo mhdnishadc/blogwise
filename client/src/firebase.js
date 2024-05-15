d9d07e757988fbd29702da8bc910f7d99bfcb434
Muhammed Nishad c <122031511+mhdnishadc@users.noreply.github.com>
2024-05-15 21:58:55 +0530

    blog

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-blog-b327f.firebaseapp.com',
-  projectId: 'mern-blog-b327f',
-  storageBucket: 'mern-blog-b327f.appspot.com',
-  messagingSenderId: '699397991367',
-  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+  authDomain: "mern-blog-d77a6.firebaseapp.com",
+  projectId: "mern-blog-d77a6",
+  storageBucket: "mern-blog-d77a6.appspot.com",
+  messagingSenderId: "384970763939",
+  appId: "1:384970763939:web:ad04867dddba91525f1916"
 };
 
 // Initialize Firebase
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,28 +4,21 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Sahand' Blog
+            About Blog Wise
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Sahand's Blog! This blog was created by Sahand Ghavidel
-              as a personal project to share his thoughts and ideas with the
-              world. Sahand is a passionate developer who loves to write about
-              technology, coding, and everything in between.
+              BlogWise is your go-to destination for weekly articles and tutorials on all things related to web development, software engineering, and programming languages. Whether you're a seasoned developer looking to expand your skill set or a newcomer eager to learn the ropes, we've got you covered.
             </p>
 
             <p>
-              On this blog, you'll find weekly articles and tutorials on topics
-              such as web development, software engineering, and programming
-              languages. Sahand is always learning and exploring new
-              technologies, so be sure to check back often for new content!
+            Our team of experienced writers and industry experts is dedicated to providing you with high-quality content that's both informative and engaging. From in-depth tutorials on the latest frameworks and technologies to insightful articles on best practices and industry trends, we strive to offer valuable resources that empower you to succeed in your coding journey.
             </p>
 
             <p>
-              We encourage you to leave comments on our posts and engage with
-              other readers. You can like other people's comments and reply to
-              them as well. We believe that a community of learners can help
-              each other grow and improve.
+            At BlogWise, we believe that learning should be accessible to everyone, which is why all our content is freely available for you to explore and enjoy. Whether you're looking to master a new programming language, enhance your web development skills, or stay up-to-date with the latest software engineering practices, you'll find everything you need right here.
+
+Join our growing community of developers, enthusiasts, and learners, and embark on a journey of continuous improvement and discovery. Together, let's unlock the endless possibilities of coding and build a brighter future through technology.
             </p>
           </div>
         </div>
